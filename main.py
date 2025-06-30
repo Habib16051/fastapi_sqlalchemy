8651c4ff0a5a60aab40f2bf8c203c8c815a87cb3
habib-technonext <habibur.rahman@technonext.com>
2025-06-30 18:36:13 +0600

    api router is added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1 import api_router
+from app.api.v1 import api
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")

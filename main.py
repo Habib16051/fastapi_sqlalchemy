af388a247b4a0247c85800d9db4752890c00eece
habib-technonext <habibur.rahman@technonext.com>
2025-07-01 10:26:19 +0600

    fixed the route error issue and project is running successfully

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app.api.v1 import api
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
-app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(api.router, prefix=settings.API_V1_STR)
 
 
 
